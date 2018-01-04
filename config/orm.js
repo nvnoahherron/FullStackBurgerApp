6f86845acf2d3bcaa4d45c58cb818ce710ff53ce
nvnoahherron <nvnoahherron@gmail.com>
2018-01-04 16:18:35 -0600

    completed orm.js and burger.js

--- config/orm.js
+++ config/orm.js
@@ -1,23 +1,46 @@
 var connection = require("./connection.js");
 
 var orm = {
-	selectAll: function(res){
+	selectAll: function(cb){
 		var queryString = "SELECT * FROM burgers";
 		connection.query(queryString, function(err, result){
-			if (err) throw err;
-
-			var burgerDevoured = [];
-			var burgerNotDevoured = [];
-
-			console.log(result);
-
-			for (var i = 0; i < result.length; i++) {
-				if (result[i].devoured) {
-					burgerDevoured.push(result[i]);
-				} else {
-					burgerNotDevoured.push(result[i]);
-				}
+			if (err) {
+				console.log(err);
 			}
+			cb(result);
 		})
 	}
-}
+	insertOne: function(newBurger, cb){
+		var queryString = "INSERT INTO burgers SET ?";
+		connection.query(queryString, newBurger, function(err, result){
+			if (err) {
+				console.log(err);
+			}
+			cb(result);
+		})
+	}
+	updateOne: function(updatedBurger, cb){
+		var queryString = "UPDATE burgers SET devoured=true WHERE ?";
+		connection.query(queryString, , updatedBurger, function(err, result){
+			if (err) {
+				console.log(err);
+			}
+			cb(result);
+		})
+	}
+};
+
+module.exports = orm;
+
+// this will go into the API routes middleware
+orm.selectAll(function(result){
+	console.log(result);
+	res.send(result);
+})
+
+app.post('/new-burger', function(req, res) {
+	// api middleware insert example
+	orm.insertOne(req.body, function(result){
+		res.send(result);
+	})
+})
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,23 @@
-var orm = require("../config/orm.js")
+var orm = require("../config/orm.js");
+
+var burger = {
+
+	selectAll: function(cb){
+		orm.selectAll(function(result){
+			cb(result);
+		});
+	},
+
+	insertOne: function(newBurger, cb){
+		orm.insertOne(newBurger, function(result){
+			cb(result);
+		});
+	},
+	updateOne: function(updatedBurger, cb){
+		orm.updateOne(updatedBurger, function(result){
+			cb(result);
+		});
+	}
+};
+
+module.exports = burger;
