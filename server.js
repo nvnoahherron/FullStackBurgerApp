698ef1dca6a0cbee49182d61ea516989461f3420
nvnoahherron <nvnoahherron@gmail.com>
2018-01-16 19:29:02 -0600

    fixing heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-var PORT = process.env ||3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 var exphbs = require("express-handlebars");
 var routes = require("./controllers/burgers_controller.js");
